5a94af09ef0d02171c2cd435c9d9184e7115c79e
sasha-sem <alex.semenenko@live.ru>
2020-04-05 16:40:26 +0300

    bug fixes

--- eco_turism/settings.py
+++ eco_turism/settings.py
@@ -52,16 +52,24 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.BrokenLinkEmailsMiddleware',
+    'django.middleware.common.CommonMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'corsheaders.middleware.CorsPostCsrfMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = [
+    'http://167.172.170.134',
+]
+CORS_ORIGIN_REGEX_WHITELIST = [
+    'http://167.172.170.134',
 ]
 
 ROOT_URLCONF = 'eco_turism.urls'
@@ -149,13 +157,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-CORS_ORIGIN_ALLOW_ALL = True
-CORS_ORIGIN_WHITELIST = ['http://167.172.170.134']
-CSRF_TRUSTED_ORIGINS = [
-    '167.172.170.134',
-]
-CORS_REPLACE_HTTPS_REFERER=True
-CORS_ORIGIN_REGEX_WHITELIST = [
-    r"^https://\167.172.170.134$",
-]
